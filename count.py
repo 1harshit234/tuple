1bce0524f08796430c5c3f43f42e0e70ca21787f
Harshit Mishra <165307122+1harshit234@users.noreply.github.com>
2024-05-22 22:09:35 -0700

    count the specific element in the tuple
    in this we have a tuple and we have count the number of occurenve of the specific element of the number of the item

---
+++ count.py
@@ -0,0 +1,27 @@
+"""l =(10,20,50,50,60)
+c = 0
+for i in l:
+    if i ==50:
+        c = c+1
+print(c)"""
+
+
+
+
+
+
+
+t = (10,50,50,5,50)
+y = t.count(50)
+print(y)
+
+
+
+
+
+
+
+
+
+
+#using count method:
